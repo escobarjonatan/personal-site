7df887197b2b3167929d4fe0a11bddbde3547bf3
Jonatan Escobar <jonatan.escobar@coxautoinc.com>
2019-11-06 22:33:50 -0600

    Adding initial assets/markup/styles to the masterpiece

---
+++ js/main.js
@@ -0,0 +1,227 @@
+(function ($) {
+	"use strict";
+
+	var fullHeight = function () {
+
+		$('.js-fullheight').css('height', $(window).height());
+		$(window).resize(function () {
+			$('.js-fullheight').css('height', $(window).height());
+		});
+	};
+	fullHeight();
+
+	// Burger Menu
+	var burgerMenu = function () {
+
+		$('body').on('click', '.js-fh5co-nav-toggle', function (event) {
+
+			event.preventDefault();
+
+			if ($('#ftco-nav').is(':visible')) {
+				$(this).removeClass('active');
+			} else {
+				$(this).addClass('active');
+			}
+		});
+	};
+	burgerMenu();
+
+	var onePageClick = function () {
+
+		$(document).on('click', '#ftco-nav a[href^="#"]', function (event) {
+			event.preventDefault();
+
+			var href = $.attr(this, 'href');
+
+			$('html, body').animate({
+				scrollTop: $($.attr(this, 'href')).offset().top - 70
+			}, 500, function () {
+				// window.location.hash = href;
+			});
+		});
+
+	};
+
+	onePageClick();
+
+	// Sticky Nav
+	var scrollWindow = function () {
+		$(window).scroll(function () {
+			var $w = $(this),
+				st = $w.scrollTop(),
+				navbar = $('.ftco_navbar'),
+				sd = $('.js-scroll-wrap');
+
+			if (st > 150) {
+				if (!navbar.hasClass('scrolled')) {
+					navbar.addClass('scrolled');
+				}
+			}
+			if (st < 150) {
+				if (navbar.hasClass('scrolled')) {
+					navbar.removeClass('scrolled sleep');
+				}
+			}
+			if (st > 350) {
+				if (!navbar.hasClass('awake')) {
+					navbar.addClass('awake');
+				}
+
+				if (sd.length > 0) {
+					sd.addClass('sleep');
+				}
+			}
+			if (st < 350) {
+				if (navbar.hasClass('awake')) {
+					navbar.removeClass('awake');
+					navbar.addClass('sleep');
+				}
+				if (sd.length > 0) {
+					sd.removeClass('sleep');
+				}
+			}
+		});
+	};
+	scrollWindow();
+
+	var contentWayPoint = function () {
+		var i = 0;
+		$('.ftco-animate').waypoint(function (direction) {
+
+			if (direction === 'down' && !$(this.element).hasClass('ftco-animated')) {
+
+				i++;
+
+				$(this.element).addClass('item-animate');
+				setTimeout(function () {
+
+					$('body .ftco-animate.item-animate').each(function (k) {
+						var el = $(this);
+						setTimeout(function () {
+							var effect = el.data('animate-effect');
+							if (effect === 'fadeIn') {
+								el.addClass('fadeIn ftco-animated');
+							} else if (effect === 'fadeInLeft') {
+								el.addClass('fadeInLeft ftco-animated');
+							} else if (effect === 'fadeInRight') {
+								el.addClass('fadeInRight ftco-animated');
+							} else {
+								el.addClass('fadeInUp ftco-animated');
+							}
+							el.removeClass('item-animate');
+						}, k * 50, 'easeInOutExpo');
+					});
+
+				}, 100);
+
+			}
+
+		}, { offset: '95%' });
+	};
+	contentWayPoint();
+
+	var goHere = function () {
+		$('.mouse-icon').on('click', function (event) {
+
+			event.preventDefault();
+
+			$('html,body').animate({
+				scrollTop: $('.goto-here').offset().top
+			}, 500, 'easeInOutExpo');
+
+			return false;
+		});
+	};
+
+	goHere();
+
+	// Imitate the typing...
+	var TxtRotate = function (el, toRotate, period) {
+		this.toRotate = toRotate;
+		this.el = el;
+		this.loopNum = 0;
+		this.period = parseInt(period, 10) || 2000;
+		this.txt = '';
+		this.tick();
+		this.isDeleting = false;
+	};
+
+	TxtRotate.prototype.tick = function () {
+		var i = this.loopNum % this.toRotate.length;
+		var fullTxt = this.toRotate[i];
+
+		if (this.isDeleting) {
+			this.txt = fullTxt.substring(0, this.txt.length - 1);
+		} else {
+			this.txt = fullTxt.substring(0, this.txt.length + 1);
+		}
+
+		this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
+
+		var that = this;
+		var delta = 300 - Math.random() * 100;
+
+		if (this.isDeleting) { delta /= 2; }
+
+		if (!this.isDeleting && this.txt === fullTxt) {
+			delta = this.period;
+			this.isDeleting = true;
+		} else if (this.isDeleting && this.txt === '') {
+			this.isDeleting = false;
+			this.loopNum++;
+			delta = 500;
+		}
+
+		setTimeout(function () {
+			that.tick();
+		}, delta);
+	};
+
+	window.onload = function () {
+		var elements = document.getElementsByClassName('txt-rotate');
+		for (var i = 0; i < elements.length; i++) {
+			var toRotate = elements[i].getAttribute('data-rotate');
+			var period = elements[i].getAttribute('data-period');
+			if (toRotate) {
+				new TxtRotate(elements[i], JSON.parse(toRotate), period);
+			}
+		}
+		// INJECT CSS
+		var css = document.createElement("style");
+		css.type = "text/css";
+		css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+		document.body.appendChild(css);
+	};
+})(jQuery);
+
+// Form Submit Handler
+document.getElementById('contact-form').addEventListener('submit', sendDataToLambda);
+// Now for the good stuff. This is the function that will send our data to AWS.
+function sendDataToLambda(e) {
+	e.preventDefault();
+	// Gets the values of each field in our form. This is the data we'll send to our Lambda function.
+	var formEmail = $("#email-input").val();
+	var formSubject = $("#name-input").val();
+	var formMessage = $("#description-input").val();
+	// This is the endpoint we created in our API Gateway. This is where we make our POST request, which calls our Lambda function.
+	var endpoint = 'https://dev.jonatanescobar.com/contact-me';
+	// Remember those form values we just grabbed? We're going to put them into an object here.
+	
+	var body = {
+		email: formEmail,
+		subject: formSubject,
+		message: formMessage
+	};
+	// Here, we instantiate our Request. This is a special object used by the Fetch API so it knows where to send data, what data to send, and how to send it.
+	var lambdaRequest = new Request(endpoint, {
+		method: 'POST',
+		// Quick note: 'no-cors' mode is for development on localhost only!
+		// mode: 'no-cors',
+		body: JSON.stringify(body)
+	});
+	// Call the Fetch API to make our request
+	fetch(lambdaRequest)
+		// This is where you can handle errors. This is just an example, so we won't cover that.
+		.then(response => console.log(response))
+		.catch(err => console.log(err));
+}
