e100d8e7d2359285d70f4f066d34c34272846441
Jonatan Escobar <jonatan.escobar@coxautoinc.com>
2019-11-06 22:54:42 -0600

    Updating old form handler

--- js/main.js
+++ js/main.js
@@ -193,35 +193,3 @@
 		document.body.appendChild(css);
 	};
 })(jQuery);
-
-// Form Submit Handler
-document.getElementById('contact-form').addEventListener('submit', sendDataToLambda);
-// Now for the good stuff. This is the function that will send our data to AWS.
-function sendDataToLambda(e) {
-	e.preventDefault();
-	// Gets the values of each field in our form. This is the data we'll send to our Lambda function.
-	var formEmail = $("#email-input").val();
-	var formSubject = $("#name-input").val();
-	var formMessage = $("#description-input").val();
-	// This is the endpoint we created in our API Gateway. This is where we make our POST request, which calls our Lambda function.
-	var endpoint = 'https://dev.jonatanescobar.com/contact-me';
-	// Remember those form values we just grabbed? We're going to put them into an object here.
-	
-	var body = {
-		email: formEmail,
-		subject: formSubject,
-		message: formMessage
-	};
-	// Here, we instantiate our Request. This is a special object used by the Fetch API so it knows where to send data, what data to send, and how to send it.
-	var lambdaRequest = new Request(endpoint, {
-		method: 'POST',
-		// Quick note: 'no-cors' mode is for development on localhost only!
-		// mode: 'no-cors',
-		body: JSON.stringify(body)
-	});
-	// Call the Fetch API to make our request
-	fetch(lambdaRequest)
-		// This is where you can handle errors. This is just an example, so we won't cover that.
-		.then(response => console.log(response))
-		.catch(err => console.log(err));
-}
